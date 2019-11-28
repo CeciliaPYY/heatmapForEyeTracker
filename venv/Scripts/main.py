a59cf28cba74f9efa6808279b5823a83f5171135
彭雨嫣 <pyy940608@gmail.com>
2019-11-28 20:29:28 +0800

    add change_cmap function which can define your own cmap

--- venv/Scripts/main.py
+++ venv/Scripts/main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+"""
+This is the main function to generate heatmap based on eye tracking data.
+"""
 
 import argparse
 import json
-import time
 
 from model.heatmap import Heatmap
-from utils.image_utils import read_image_return_nparray, image_add_with_certain_weight, get_image_dpi
+from utils.image_utils import read_image_return_nparray, image_add_with_certain_weight, get_image_dpi, change_cmap
 
 
 def load_eye_tracking_data(fname, mode='r'):
+    """
+    Load eye tracking data from file named fname
+    :param fname: file path and name
+    :param mode: read or write
+    :return: eye tracking data
+    """
     with open(fname, mode) as f:
         heatmap_data = json.load(f)
 
@@ -17,6 +25,14 @@ def load_eye_tracking_data(fname, mode='r'):
 
 
 def main(json_path, background_image_path, heatmap_save_path, alpha):
+    """
+    main function to generate heatmap
+    :param json_path: json file saved eye tracking data
+    :param background_image_path: background shelf image path
+    :param heatmap_save_path: where to save heatmap
+    :param alpha: heatmap = alpha * foreground_image + background_image
+    :return: void
+    """
 
     eye_tracking_data = load_eye_tracking_data(json_path)
 
@@ -24,16 +40,12 @@ def main(json_path, background_image_path, heatmap_save_path, alpha):
     foreground_heatmap = heatmap.generate_heatmap()
 
     background_image = read_image_return_nparray(background_image_path)
+    cmap = change_cmap()
 
-    image_add_with_certain_weight(foreground_heatmap, background_image, alpha=alpha, save=heatmap_save_path)
+    image_add_with_certain_weight(foreground_heatmap, background_image, alpha=alpha, cmap=cmap, save=heatmap_save_path)
 
 
 if __name__ == "__main__":
-
-    split_line = '=' * 100
-    print(split_line)
-    startTimeStamp = int(round(time.time() * 1000))
-    print("  *** Begin : %s ***" % time.ctime())
 
     parser = argparse.ArgumentParser(add_help=False, description="Generate Heatmap for Eye Tracking Data.")
     parser.add_argument('-h', '--help', default=argparse.SUPPRESS,
@@ -42,7 +54,7 @@ if __name__ == "__main__":
                         help='path to heatmap original data')
     parser.add_argument('-bg', '--background_image', type=str, default="./background_image.png",
                         help='path to background image')
-    parser.add_argument('-a', '--alpha', type=float, default=0.5,
+    parser.add_argument('-a', '--alpha', type=float, default=0.4,
                         help='alpha percent to add heatmap to background image which is a float between 0 and 1')
     parser.add_argument('-o', '--output', type=str, default="./heatmap.png",
                         help='path to save heatmap image')
@@ -50,9 +62,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.json, args.background_image, args.output, args.alpha)
-
-    endTimeStamp = int(round(time.time() * 1000))
-    print('  *** End : %s ***\n\n' % time.ctime())
-    print("Time Duration is {}".format(endTimeStamp - startTimeStamp))
+    print(args.output)
 
 
